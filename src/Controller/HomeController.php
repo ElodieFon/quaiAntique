3fbfb8aa23ebd19f3886ba9fbe75a35680fb1300
ElodieFon <e.fontaine.1991@hotmail.fr>
2023-02-21 11:36:03 +0100

    ajout de style

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $titlePage = "Le Quai Antique";
+        $subtitle = "Faites Voyager Vos Papilles";
+        
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'titlePage' => $titlePage , 'subtitle' => $subtitle
         ]);
     }
 }
