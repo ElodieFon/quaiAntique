340fe9cfa173d21a666c5ea2e8965d3c6f7f8fff
ElodieFon <e.fontaine.1991@hotmail.fr>
2023-02-21 16:27:33 +0100

    création de la page de contact , du form contact  et du style contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request ;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request , MailerInterface $mailer): Response
     {
+        $titlePage = "Nous Contacter";
+      	$subtitle = "";
+		$sucessMessage = "";
+        	
+		$form = $this-> createForm(ContactType::class );
+		$form->handleRequest($request);
+
+
+		if($form->isSubmitted() && $form->isValid()){
+			$contact = $form->getData();
+
+			$adress = $contact['email'];
+			$message = $contact['message'];
+			
+			$email = (new Email())
+			->from($adress)
+			->to('admin@admin.com')
+			->subject('Demande de contact')
+			->text($message);
+
+			$mailer->send($email);
+
+			$sucessMessage = "votre message a bien était envoyé !";
+		}
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'titlePage'=>$titlePage , 
+            'subtitle' => $subtitle ,
+            'form' => $form->createView(),
+			'message' => $sucessMessage
         ]);
     }
 }
