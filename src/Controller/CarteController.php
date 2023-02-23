f75647cd504faa79969a42bd6409984345a4b153
ElodieFon <e.fontaine.1991@hotmail.fr>
2023-02-23 22:08:31 +0100

    affichage des produits

--- src/Controller/CarteController.php
+++ src/Controller/CarteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class CarteController extends AbstractController
 {
     #[Route('/carte', name: 'app_carte')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
         $titlePage = "Nos menu";
         $subtitle = "";
-
+        $products = $productRepository->findAll();
         return $this->render('carte/index.html.twig', [
             'titlePage'=>$titlePage , 
-            'subtitle' => $subtitle
-            
+            'subtitle' => $subtitle ,
+            'products' => $products
         ]);
     }
 }
