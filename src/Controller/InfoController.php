f965289f10cf0cb07858d8f380d39c6c46c41ada
ElodieFon <e.fontaine.1991@hotmail.fr>
2023-02-27 15:51:03 +0100

    apelle de l'entité article dans la page info

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use App\Repository\HoraireRepository;
+use App\Repository\InfoRestoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +12,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class InfoController extends AbstractController
 {
     #[Route('/info', name: 'app_info')]
-    public function index(): Response
+    public function index(
+        HoraireRepository $horaire ,
+        InfoRestoRepository $infoResto, 
+        ArticleRepository $article
+    ): Response
     {
+        $infosResto = $infoResto->findAll();
+        $horaires = $horaire->findAll();
+        $articles = $article->findAll();
         $titlePage = "A Propos";
         $subtitle = "";
         return $this->render('info/index.html.twig', [
+            'articles'=> $articles,
+            'infosResto'=>$infosResto,
+            'horaires' => $horaires,
             'titlePage'=>$titlePage , 
             'subtitle' => $subtitle
         ]);
