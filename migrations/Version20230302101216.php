fdc55b185e7336172ac47f252a5ea65cbee6c532
ElodieFon <e.fontaine.1991@hotmail.fr>
2023-03-03 08:08:37 +0100

    migration

--- migrations/Version20230302101216.php
+++ migrations/Version20230302101216.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230228222710 extends AbstractMigration
+final class Version20230302101216 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,16 @@ final class Version20230228222710 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reservation CHANGE nbpersons nb_persons INT NOT NULL');
+        $this->addSql('ALTER TABLE reservation ADD CONSTRAINT FK_42C84955A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_42C84955A76ED395 ON reservation (user_id)');
+        $this->addSql('ALTER TABLE user ADD name VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reservation CHANGE nb_persons nbpersons INT NOT NULL');
+        $this->addSql('ALTER TABLE reservation DROP FOREIGN KEY FK_42C84955A76ED395');
+        $this->addSql('DROP INDEX IDX_42C84955A76ED395 ON reservation');
+        $this->addSql('ALTER TABLE user DROP name');
     }
 }
