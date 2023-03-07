e5ed46bdd60277c4a5edb8cd6a7f55e9c4bbbf85
elodie <e.fontaine.1991@hotmail.fr>
2023-03-07 14:58:39 +0100

    mise a jour importation sql depui phpmyadmin

--- lequaiantique.sql
+++ lequaiantique.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 06 mars 2023 à 16:27
+-- Généré le : mar. 07 mars 2023 à 13:15
 -- Version du serveur : 5.7.40
 -- Version de PHP : 8.1.13
 
@@ -36,7 +36,16 @@ CREATE TABLE IF NOT EXISTS `article` (
   `inverse` tinyint(1) DEFAULT NULL,
   `active` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `article`
+--
+
+INSERT INTO `article` (`id`, `name`, `description`, `image`, `inverse`, `active`) VALUES
+(1, 'Histoire', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', 'chetan-hireholi-WOQCwfnlhcs-unsplash.jpg', 0, 1),
+(2, 'Notre cuisine', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', 'cuisine.jpg', 1, 1),
+(3, 'Notre chef', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', 'chef.jpg', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -52,7 +61,17 @@ CREATE TABLE IF NOT EXISTS `category` (
   `updated_at` datetime DEFAULT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `category`
+--
+
+INSERT INTO `category` (`id`, `nom`, `active`, `updated_at`, `created_at`) VALUES
+(1, 'Entrées', 1, NULL, '2023-03-07 13:06:02'),
+(2, 'Plats', 1, NULL, '2023-03-07 13:06:13'),
+(3, 'Desserts', 1, NULL, '2023-03-07 13:06:23'),
+(4, 'Goutés', 0, NULL, '2023-03-07 13:06:43');
 
 -- --------------------------------------------------------
 
@@ -73,7 +92,7 @@ CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
 --
 
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20230306142159', '2023-03-06 14:22:05', 380);
+('DoctrineMigrations\\Version20230306142159', '2023-03-07 08:02:40', 513);
 
 -- --------------------------------------------------------
 
@@ -186,7 +205,23 @@ CREATE TABLE IF NOT EXISTS `product` (
   `product_of_moment` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `IDX_D34A04AD12469DE2` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `product`
+--
+
+INSERT INTO `product` (`id`, `category_id`, `name`, `description`, `prix`, `image`, `updated_at`, `active`, `created_at`, `product_of_moment`) VALUES
+(1, 3, 'crepes suzette', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '250.00', 'joyful-vT5xrj3z1OE-unsplash.jpg', NULL, 1, '2023-03-07 13:07:11', 1),
+(2, 2, 'blanquette de veaux', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '1500.00', 'farhad-ibrahimzade-KpOl9jV2aJM-unsplash (1).jpg', NULL, 1, '2023-03-07 13:07:42', 0),
+(3, 1, 'salade fruits de mer', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '650.00', 'anna-pelzer-IGfIGP5ONV0-unsplash (1).jpg', '2023-03-07 13:08:54', 1, '2023-03-07 13:08:20', 0),
+(4, 1, 'salade César', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '650.00', 'hermes-rivera-OzBLe_Eg1mg-unsplash (1).jpg', NULL, 1, '2023-03-07 13:08:49', 1),
+(5, 2, 'bœuf bourguignon', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '1500.00', 'farhad-ibrahimzade-TvMWBS6TIsg-unsplash (1).jpg', NULL, 1, '2023-03-07 13:09:19', 1),
+(6, 2, 'sautée de fruit de mer', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '1420.00', 'farhad-ibrahimzade-HNmcgpzPHag-unsplash.jpg', NULL, 1, '2023-03-07 13:09:56', 0),
+(7, 3, 'Gateau au yaourt', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '350.00', 'téléchargement.jpg', '2023-03-07 13:10:45', 0, '2023-03-07 13:10:30', 0),
+(8, 3, 'mini glaces', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '350.00', 'najlacam-f8TajCVv4s8-unsplash (1).jpg', NULL, 1, '2023-03-07 13:11:15', 0),
+(9, 3, 'Tarte citron meringué', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '350.00', '13on-z4BJGODf6O4-unsplash.jpg', '2023-03-07 13:11:57', 1, '2023-03-07 13:11:34', 1),
+(10, 1, 'Salade mixte', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci eveniet consectetur illum labore excepturi eos libero nemo impedit unde quibusdam dolorem pariatur, fuga autem ea, nobis numquam! Dolores delectus ducimus tenetur provident sapiente minima dolorum quod nostrum quo corrupti ipsam, rerum, laboriosam, soluta facere ad optio blanditiis! Cupiditate, nisi debitis!', '650.00', 'anh-nguyen-kcA-c3f_3FE-unsplash.jpg', NULL, 1, '2023-03-07 13:12:49', 0);
 
 -- --------------------------------------------------------
 
@@ -208,16 +243,16 @@ CREATE TABLE IF NOT EXISTS `reservation` (
   PRIMARY KEY (`id`),
   KEY `IDX_42C84955F2A733EB` (`heure_id`),
   KEY `IDX_42C84955A76ED395` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Déchargement des données de la table `reservation`
 --
 
 INSERT INTO `reservation` (`id`, `heure_id`, `user_id`, `name_client`, `date`, `nb_persons`, `nb_children`, `comentaire`, `allergie`) VALUES
-(1, 1, 1, 'heello', '2023-03-18 00:00:00', 5, 5, NULL, 0),
-(2, 1, 1, 'heello', '2023-03-15 00:00:00', 5, 0, NULL, 0),
-(3, 1, 1, 'heello', '2023-03-09 00:00:00', 5, 0, NULL, 0);
+(7, 1, 1, 'hello', '2023-03-07 00:00:00', 2, 0, NULL, 0),
+(8, 1, 1, 'hello', '2023-03-07 00:00:00', 6, 0, NULL, 0),
+(9, 1, 1, 'hello', '2023-03-07 00:00:00', 2, 0, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -239,7 +274,7 @@ CREATE TABLE IF NOT EXISTS `reservation_horaire` (
 --
 
 INSERT INTO `reservation_horaire` (`id`, `heure`, `active`, `nb_place`) VALUES
-(1, '11:30:00', 1, 2),
+(1, '11:30:00', 1, 10),
 (2, '11:45:00', 1, 10),
 (3, '12:00:00', 1, 10),
 (4, '12:15:00', 1, 10),
